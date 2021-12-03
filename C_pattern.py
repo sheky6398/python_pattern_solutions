af1dad31dc8963ec3f6ce3ed05e92788474f17b9
Bharat <sheky6398@gmail.com>
2021-12-03 15:39:44 +0530

    c pattern add

--- C_pattern.py
+++ C_pattern.py
@@ -1,10 +1,12 @@
 # C Pattern 
-#   * * * * * 
-# *           
-# *           
-# *           
-# *           
-#   * * * * * 
+#   * * * * * * * 
+# *               
+# *               
+# *               
+# *               
+# *               
+# *               
+#   * * * * * * * 
 n = int(input("Enter the number: "))
 for i in range(1,n+1):
     for j in range(1,n+1):
